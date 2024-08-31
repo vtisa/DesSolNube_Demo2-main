2861d1ea459e8a7003963a3e25c274e658fefbd7
V.T.isa <135535849+vtisa@users.noreply.github.com>
2024-08-30 22:50:51 -0500

    update

--- conexion.php
+++ conexion.php
@@ -1,5 +1,4 @@
 <?php
-<?php
 $db = pg_connect("host=dpg-cr6g05bv2p9s7392u7h0-a.oregon-postgres.render.com port=5432 dbname=dbprueba_7k2p user=dbprueba_7k2p_user password=H0IB7ht78vEte22UZ2eQEUcYWSvLjtn4");
 if ($db) {
     echo "Conexión exitosa.";
