bc016122a5c9fccd9deabc2c19e4f6cccb3392f1
V.T.isa <135535849+vtisa@users.noreply.github.com>
2024-08-31 14:08:21 -0500

    update

--- index-post.php
+++ index-post.php
@@ -2,25 +2,14 @@
 include("conexion.php");
 $con = conexion();
 
-if ($con) {
-    $doc = $_POST["doc"];
-    $nom = $_POST["nom"];
-    $ape = $_POST["ape"];
-    $dir = $_POST["dir"];
-    $cel = $_POST["cel"];
+$doc = $_POST["doc"];
+$nom = $_POST["nom"];
+$ape = $_POST["ape"];
+$dir = $_POST["dir"];
+$cel = $_POST["cel"];
 
-    $sql = "INSERT INTO Persona (documento, nombre, apellido, direccion, celular) VALUES ('$doc', '$nom', '$ape', '$dir', '$cel')";
-    
-    $result = pg_query($con, $sql);
+$sql = "insert into persona values(default,'$doc','$nom','$ape','$dir','$cel')";
+pg_query($con, $sql);
 
-    if (!$result) {
-        die("Error en la consulta SQL: " . pg_last_error());
-    } else {
-        echo "Datos insertados correctamente.<br>";
-    }
-
-    //header("Location: listar_grupoYCG.php");
-} else {
-    echo "No se pudo establecer la conexion.<br>";
-}
+//header("location:listar_grupoYCG.php");
 ?>
