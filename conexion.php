96e1e01328402c43635e111b3063bcb68736823a
V.T.isa <135535849+vtisa@users.noreply.github.com>
2024-08-31 13:55:48 -0500

    update

--- conexion.php
+++ conexion.php
@@ -1,15 +1,19 @@
 <?php
+function conexion() {
+    $host = "host=dpg-cr6g05bv2p9s7392u7h0-a.oregon-postgres.render.com";
+    $port = "port=5432";
+    $dbname = "dbname=dbprueba_7k2p";
+    $user = "user=dbprueba_7k2p_user";
+    $password = "password=H0IB7ht78vEte22UZ2eQEUcYWSvLjtn4";
 
-function conexion(){
+    $db = pg_connect("$host $port $dbname $user $password");
 
-$host = "host=dpg-cr6g05bv2p9s7392u7h0-a.oregon-postgres.render.com";
-$port = "port=5432";
-$dbname = "dbname=dbprueba_7k2p";
-$user = "user=dbprueba_7k2p_user";
-$password = "password=H0IB7ht78vEte22UZ2eQEUcYWSvLjtn4";
+    if (!$db) {
+        die("Error: No se pudo conectar a la base de datos. " . pg_last_error());
+    } else {
+        echo "Conexión exitosa.<br>";
+    }
 
-$db = pg_connect("$host $port $dbname $user $password");
-
-return $db;
+    return $db;
 }
 ?>
--- index-post.php
+++ index-post.php
@@ -2,14 +2,25 @@
 include("conexion.php");
 $con = conexion();
 
-$doc = $_POST["doc"];
-$nom = $_POST["nom"];
-$ape = $_POST["ape"];
-$dir = $_POST["dir"];
-$cel = $_POST["cel"];
+if ($con) {
+    $doc = $_POST["doc"];
+    $nom = $_POST["nom"];
+    $ape = $_POST["ape"];
+    $dir = $_POST["dir"];
+    $cel = $_POST["cel"];
 
-$sql = "insert into persona values(default,'$doc','$nom','$ape','$dir','$cel')";
-pg_query($con, $sql);
+    $sql = "INSERT INTO Persona (documento, nombre, apellido, direccion, celular) VALUES ('$doc', '$nom', '$ape', '$dir', '$cel')";
+    
+    $result = pg_query($con, $sql);
 
-header("location:listar_grupoYCG.php");
+    if (!$result) {
+        die("Error en la consulta SQL: " . pg_last_error());
+    } else {
+        echo "Datos insertados correctamente.<br>";
+    }
+
+    header("Location: listar_grupoYCG.php");
+} else {
+    echo "No se pudo establecer la conexión.<br>";
+}
 ?>
