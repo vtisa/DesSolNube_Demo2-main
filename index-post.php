0343961bd4a4f61a37a55a0caa4f38670d63e13f
V.T.isa <135535849+vtisa@users.noreply.github.com>
2024-08-31 10:09:00 -0500

    update

--- index-post.php
+++ index-post.php
@@ -2,22 +2,14 @@
 include("conexion.php");
 $con = conexion();
 
-$documento = $_POST["documento"];
-$nombre = $_POST["nombre"];
-$apellido = $_POST["apellido"];
-$direccion = $_POST["direccion"];
-$celular = $_POST["celular"];
+$doc = $_POST["doc"];
+$nom = $_POST["nom"];
+$ape = $_POST["ape"];
+$dir = $_POST["dir"];
+$cel = $_POST["cel"];
 
-$query = "INSERT INTO persona (documento, nombre, apellido, direccion, celular) 
-          VALUES ($1, $2, $3, $4, $5)";
+$sql = "insert into persona values(default,'$doc','$nom','$ape','$dir','$cel')";
+pg_query($con, $sql);
 
-pg_prepare($con, "insert_persona", $query);
-
-$result = pg_execute($con, "insert_persona", array($documento, $nombre, $apellido, $direccion, $celular));
-
-if ($result) {
-    header("Location: listar_grupoYCG.php");
-} else {
-    echo "Error al insertar: " . pg_last_error($con);
-}
+header("location:listar_grupoYCG.php");
 ?>
