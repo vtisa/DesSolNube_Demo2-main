bdc52231bba7b2c39af6a521928fccbf09811ca0
V.T.isa <135535849+vtisa@users.noreply.github.com>
2024-08-30 22:44:08 -0500

    update

--- index-post.php
+++ index-post.php
@@ -13,23 +13,3 @@ pg_query($con, $sql);
 
 header("location:listar_grupoYCG.php");
 ?>
-<?php
-include("conexion.php");
-$con = conexion();
-
-// Obtener los valores del formulario
-$doc = $_POST["doc"];
-$nom = $_POST["nom"];
-$ape = $_POST["ape"];
-$dir = $_POST["dir"];
-$cel = $_POST["cel"];
-
-// Crear la consulta SQL, especificando los nombres de las columnas
-$sql = "INSERT INTO \"Persona\" (documento, nombre, apellido, direccion, celular) VALUES ('$doc', '$nom', '$ape', '$dir', '$cel')";
-
-// Ejecutar la consulta
-pg_query($con, $sql);
-
-// Redirigir a la página de listado
-header("Location: listar_grupoYCG.php");
-?>
