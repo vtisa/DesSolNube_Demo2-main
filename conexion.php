ffcbeb6f526e7f534b114ab2fc377cdc770db4f5
V.T.isa <135535849+vtisa@users.noreply.github.com>
2024-08-31 14:13:14 -0500

    update

--- conexion.php
+++ conexion.php
@@ -1,19 +1,15 @@
 <?php
-function conexion() {
-    $host = "host=dpg-cr6g05bv2p9s7392u7h0-a.oregon-postgres.render.com";
-    $port = "port=5432";
-    $dbname = "dbname=dbprueba_7k2p";
-    $user = "user=dbprueba_7k2p_user";
-    $password = "password=H0IB7ht78vEte22UZ2eQEUcYWSvLjtn4";
 
-    $db = pg_connect("$host $port $dbname $user $password");
+function conexion(){
 
-    if (!$db) {
-        die("Error: No se pudo conectar a la base de datos. " . pg_last_error());
-    } else {
-        echo "Conexion exitosa.<br>";
-    }
+$host = "host=dpg-cr6g05bv2p9s7392u7h0-a.oregon-postgres.render.com";
+$port = "port=5432";
+$dbname = "dbname=dbprueba_7k2p";
+$user = "user=dbprueba_7k2p_user";
+$password = "password=H0IB7ht78vEte22UZ2eQEUcYWSvLjtn4";
 
-    return $db;
+$db = pg_connect("$host $port $dbname $user $password");
+
+return $db;
 }
 ?>
