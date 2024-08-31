e55e030461d5ad7f6f9fb4c9b386e34e0ce6e278
V.T.isa <135535849+vtisa@users.noreply.github.com>
2024-08-31 09:40:50 -0500

    update

--- conexion.php
+++ conexion.php
@@ -8,7 +8,7 @@ $dbname = "dbname=dbprueba_7k2p";
 $user = "user=dbprueba_7k2p_user";
 $password = "password=H0IB7ht78vEte22UZ2eQEUcYWSvLjtn4";
 
-$db = pg_connect("$host $port $dbname $user $password");
+$db = pg_connect("host=$host;port=$port;dbname=$dbname;user=$user;password=$password");
 
 return $db;
 }
