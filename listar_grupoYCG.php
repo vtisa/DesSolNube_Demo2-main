1ec6a722fa13d3da7a7c6700c8e131733c1a4e9f
V.T.isa <135535849+vtisa@users.noreply.github.com>
2024-08-31 18:19:38 -0500

    update

--- listar_grupoYCG.php
+++ listar_grupoYCG.php
@@ -1,4 +1,44 @@
+<?php
+include("conexion.php");
+$con = conexion();
 
+// Handle form submissions
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['action'])) {
+        if ($_POST['action'] == 'edit') {
+            $documento = $_POST['documento'];
+            $nombre = $_POST['nombre'];
+            $apellido = $_POST['apellido'];
+            $direccion = $_POST['direccion'];
+            $celular = $_POST['celular'];
+            
+            $sql = "UPDATE persona SET nombre = $1, apellido = $2, direccion = $3, celular = $4 WHERE documento = $5";
+            $result = pg_query_params($con, $sql, array($nombre, $apellido, $direccion, $celular, $documento));
+            
+            if (!$result) {
+                echo "Error al actulizar" . pg_last_error($con);
+            } else {
+                echo "Actualizado exitosamente";
+            }
+        } elseif ($_POST['action'] == 'delete') {
+            $documento = $_POST['documento'];
+            
+            $sql = "DELETE FROM persona WHERE documento = $1";
+            $result = pg_query_params($con, $sql, array($documento));
+            
+            if (!$result) {
+                echo "Error al borrar: " . pg_last_error($con);
+            } else {
+                echo "Borrado exitosamente";
+            }
+        }
+    }
+}
+
+// Fetch all records
+$sql = "SELECT * FROM persona";
+$resultado = pg_query($con, $sql);
+?>
 
 <!doctype html>
 <html lang="es">
